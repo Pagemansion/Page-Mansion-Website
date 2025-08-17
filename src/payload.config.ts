609d2095670838b4267cdfddeef08c21ba83057f
David Bradford <davidbradforduko@yahoo.com>
2025-08-17 11:31:05 +0100

    trying fixes for images admin

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,6 @@
 // storage-adapter-import-placeholder
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
+import  { vercelBlobStorage }  from '@payloadcms/storage-vercel-blob'
 
 import sharp from 'sharp' // sharp-import
 import path from 'path'
@@ -68,6 +69,14 @@ export default buildConfig({
   plugins: [
     ...plugins,
     // storage-adapter-placeholder
+    vercelBlobStorage({
+      enabled: true,
+      collections: {
+        // If you have another collection that supports uploads, you can add it below
+        media: true,
+      },
+      token: process.env.BLOB_READ_WRITE_TOKEN,
+    }),
   ],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
